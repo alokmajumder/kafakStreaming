ff919bc3fba8cd36d40a70a4e5b376fac8000ebf
alokmajumder <apu.sweden@gmail.com>
2019-04-23 12:02:02 +0200

    kafka streaming 1st

---
+++ src/main/java/com/ibm/ikea/kafka/stream/empaggregate.java
@@ -0,0 +1,34 @@
+package com.ibm.ikea.kafka.stream;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+
+import java.util.Properties;
+
+public class empaggregate {
+    public static void main(String[] args) {
+        System.out.println("Hello Alok");
+        Properties prop = new Properties();
+        prop.put(StreamsConfig.APPLICATION_ID_CONFIG,"streams-emp-start");
+        prop.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"10.164.0.16:9082");
+        prop.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        prop.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        prop.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KStreamBuilder builder = new KStreamBuilder();
+        KStream<String,String> emprecord = builder.stream("avro-schema-test");
+        emprecord.to("avro-schema-out");
+        KafkaStreams streams = new KafkaStreams(builder, prop);
+        streams.cleanUp();
+        streams.start();
+        System.out.println(streams.toString());
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+
+    }
+
+}
