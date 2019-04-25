9d898076ed5fd7cc1642edebc0753c3a9d682c72
alokmajumder <apu.sweden@gmail.com>
2019-04-25 18:13:22 +0200

    wiki

--- src/main/java/com/ibm/ikea/kafka/stream/WikipediaFeedAvroLambdaExample.java
+++ src/main/java/com/ibm/ikea/kafka/stream/WikipediaFeedAvroLambdaExample.java
@@ -1,27 +1,28 @@
 package com.ibm.ikea.kafka.stream;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 
+
 import java.util.Properties;
 
-public class empsalary {
+public class WikipediaFeedAvroLambdaExample {
     public static void main(String[] args) {
         Properties prop = new Properties();
-        prop.put(StreamsConfig.APPLICATION_ID_CONFIG,"streams-emp-start");
+        prop.put(StreamsConfig.APPLICATION_ID_CONFIG, "dept-avro-lambda-example");
         prop.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"10.164.0.16:9082");
-        prop.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        prop.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "10.164.0.16:8081");
         prop.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         prop.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         KStreamBuilder builder = new KStreamBuilder();
-        KStream<String,String> emprecord = builder.stream("avro-schema-test");
-        KStream<String, String> empfilter = emprecord.filter((key, value) -> value.contains("Anshul"));
-        emprecord.to("emp-topic-filter");
+        KStream<String, emprecord> feeds = builder.stream("avro-schema-test");
+        feeds.to("salary-avro-test");
         KafkaStreams streams = new KafkaStreams(builder, prop);
         streams.cleanUp();
         streams.start();
